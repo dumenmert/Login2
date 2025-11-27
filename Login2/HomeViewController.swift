788d21f30df109d4ab8cfbd7440b8cf4627c355d
dumenmert <mertdumen2003@gmail.com>
2025-11-27 08:39:35 +0300

    update

--- Login2/HomeViewController.swift
+++ Login2/HomeViewController.swift
@@ -2,28 +2,43 @@
 //  HomeViewController.swift
 //  Login2
 //
-//  Created by Mert Dümen on 20.11.2025.
+//  Created by Mert Dümen on 23.11.2025.
 //
 
 import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var loginText1: UILabel!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var loginText: UILabel!
+    @IBOutlet weak var TextView: UITextView!
+    var counter = 0
+    
+    @IBOutlet weak var SplashView: UIView!
+    var number = 1
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // 2 saniye bekle sonra kaybolsun
+        UIView.animate(withDuration: 1.0, delay: 2.0, options: []) {
+            self.SplashView.alpha = 0.0
+        } completion: { _ in
+            self.SplashView.isHidden = true
+        }
     }
-    */
-
-}
+    @IBAction func btn1Pressed(_ sender: Any) {
+        counter += 1
+        TextView.text += "Mert \(counter)\n"
+        
+        
+    }
+    
+        }
+    
